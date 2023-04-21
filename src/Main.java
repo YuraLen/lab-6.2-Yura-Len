77069d99b07613d4d5b349a6f93be79b6ddc6331
YuraLen <92933581+YuraLen@users.noreply.github.com>
2023-04-21 10:14:56 +0300

    update

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        ModelOfCar BMW5Series = new ModelOfCar("BMW", "5 series");
-        ModelOfCar MercedesC63 = new ModelOfCar("Mercedes-Benz", "C63");
-        ModelOfCar AudiA6 = new ModelOfCar("Audi", "A6");
+        ModelOfCar bmw5Series = new ModelOfCar("BMW", "5 series");
+        ModelOfCar mercedesC63 = new ModelOfCar("Mercedes-Benz", "C63");
+        ModelOfCar audiA6 = new ModelOfCar("Audi", "A6");
 
-        EngineOfCar BMW5SeriesBaseEngine = new EngineOfCar("B48B20", 2.0, 249, 9000);
-        EngineOfCar BMW5SeriesPremiumEngine = new EngineOfCar("B58B30", 3.0, 340, 15000);
-        EngineOfCar MercedesC63BaseEngine = new EngineOfCar("M139", 2.0, 470, 27000);
-        EngineOfCar MercedesC63PremiumEngine = new EngineOfCar("M313", 4.0, 592, 35000);
-        EngineOfCar AudiA6BaseEngine = new EngineOfCar("A414B2", 1.8, 220, 10000);
-        EngineOfCar AudiA6PremiumEngine = new EngineOfCar("A612B12", 4.2, 421, 19500);
+        EngineOfCar bmw5SeriesBaseEngine = new EngineOfCar("B48B20", 2.0, 249, 9000);
+        EngineOfCar bmw5SeriesPremiumEngine = new EngineOfCar("B58B30", 3.0, 340, 15000);
+        EngineOfCar mercedesC63BaseEngine = new EngineOfCar("M139", 2.0, 470, 27000);
+        EngineOfCar mercedesC63PremiumEngine = new EngineOfCar("M313", 4.0, 592, 35000);
+        EngineOfCar audiA6BaseEngine = new EngineOfCar("A414B2", 1.8, 220, 10000);
+        EngineOfCar audiA6PremiumEngine = new EngineOfCar("A612B12", 4.2, 421, 19500);
 
-        SalonEquipment BMW5SeriesBaseEquipment = new SalonEquipment("в базовій комплектації", 30000, "МКПП");
-        SalonEquipment BMW5SeriesPremiumEquipment = new SalonEquipment("в преміум комплектації", 50000, "АКПП");
-        SalonEquipment MercedesC63BaseEquipment = new SalonEquipment("в базовій комплектації", 50000, "АКПП");
-        SalonEquipment MercedesC63PremiumEquipment = new SalonEquipment("в преміум комплектації", 70000, "АКПП");
-        SalonEquipment AudiA6BaseEquipment = new SalonEquipment("в базовій комплектації", 35000, "МКПП");
-        SalonEquipment AudiA6PremiumEquipment = new SalonEquipment("в мреміум комплектації", 52000, "АКПП");
+        SalonEquipment bmw5SeriesBaseEquipment = new SalonEquipment("в базовій комплектації", 30000, "МКПП");
+        SalonEquipment bmw5SeriesPremiumEquipment = new SalonEquipment("в преміум комплектації", 50000, "АКПП");
+        SalonEquipment mercedesC63BaseEquipment = new SalonEquipment("в базовій комплектації", 50000, "АКПП");
+        SalonEquipment mercedesC63PremiumEquipment = new SalonEquipment("в преміум комплектації", 70000, "АКПП");
+        SalonEquipment audiA6BaseEquipment = new SalonEquipment("в базовій комплектації", 35000, "МКПП");
+        SalonEquipment audiA6PremiumEquipment = new SalonEquipment("в мреміум комплектації", 52000, "АКПП");
+
+        PriceOfCar finalp = new PriceOfCar();
+        finalp.price = bmw5SeriesBaseEngine.enginePrice + bmw5SeriesPremiumEquipment.priceOfEquipment;
+
 
 
         System.out.printf("Обираючи %s %s %s, Ви отримаєте автомобіль на %s з мотором %s, об'м якого %.1fл. та %d кінських сил. Вартість автомобіля буде %d$ .%n",
-                BMW5Series.brand, BMW5Series.model, BMW5SeriesPremiumEquipment.classOfEquipment, BMW5SeriesPremiumEquipment.transmision,
-                BMW5SeriesPremiumEngine.modelOfEngine, BMW5SeriesPremiumEngine.capacity, BMW5SeriesPremiumEngine.horsePower,
-                BMW5SeriesPremiumEquipment.priceOfEquipment + BMW5SeriesPremiumEngine.enginePrice);
+                bmw5Series.brand, bmw5Series.model, bmw5SeriesPremiumEquipment.classOfEquipment, bmw5SeriesPremiumEquipment.transmision,
+                bmw5SeriesPremiumEngine.modelOfEngine, bmw5SeriesPremiumEngine.capacity, bmw5SeriesPremiumEngine.horsePower, finalp.price);
     }
 }
--- src/ModelOfCar.java
+++ src/ModelOfCar.java
@@ -2,9 +2,12 @@ public class ModelOfCar {
     String brand;
     String model;
 
-
     public ModelOfCar(String brand, String model){
         this.brand = brand;
         this.model = model;
     }
+}
+
+class PriceOfCar{
+    int price;
 }
